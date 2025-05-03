70877e1d94a769ccc18b563e5d3d9c590385b3e9
Pegah Tajvidi <pegahtajvidi82@gmail.com>
2025-05-02 19:47:03 -0700

    made start menu, added functionality for new game and quit game. added new fonts.

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void onStartClick()
+    {
+        // Load the game scene
+        SceneManager.LoadScene("TestScene");
+    }
+    public void onQuitClick()
+    {
+        // Quit the game
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false; // Stop playing the scene in the editor
+#endif
+        Application.Quit();
+    }
+
+    public void onSettingsClick()
+    {
+        // Load the settings scene
+        Debug.Log("Settings button clicked");
+    }
+
+    public void onContinueClick()
+    {
+        // Load the last saved game
+        Debug.Log("Continue button clicked");
+    }
+}
